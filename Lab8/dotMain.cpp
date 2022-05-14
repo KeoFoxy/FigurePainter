9f4a5b954bb48eff261408544b574507d72552e2
KeoFoxy <arasdiel68@gmail.com>
2022-05-14 13:31:08 +0300

    Full lab8

--- Lab8/dotMain.cpp
+++ Lab8/dotMain.cpp
@@ -1,12 +1,36 @@
 #include "dotOnSurface.h"
 
+template<typename A>
+
+A avgValue(std::vector<A> xyValue)
+{
+    
+    A sum{};
+    
+    for(auto &item: xyValue)
+    {
+        sum = sum + item;
+    }
+    
+    A res = sum;
+    return res/ static_cast<int>(xyValue.size());
+}
+
 int main()
 {
-    dotOnSurface<int> dot;
-    dot.setCoordinates(3, 4);
-    for(int i = 0; i < 2; i++)
-    {
-        std::cout<<dot.getCoordinates()[i]<< std::endl;
-    }
+    std::vector<float> coords = {1, 2, 3, 4};
+
+    std::vector<int> MintValues = {-2, -2, -2};
+    std::vector<int> intValues = {1, 2 ,3, 4, 5, 6};
+    std::vector<float> floatValues = {1.23, 2.45};
+    std::vector<double> doubleValues = {1.253, 2.4325, 3.32314, 2.732231};
+
+    std::cout<< "Average value of -int vector: " << avgValue(MintValues) <<std::endl;
+    std::cout<< "Average value of int vector: " << avgValue(intValues) <<std::endl;
+    std::cout<< "Average value of float vector: " << avgValue(floatValues)<<std::endl;
+    std::cout<< "Average value of double vector: " << avgValue(doubleValues)<<std::endl;
+
+
+
     return 0;
 }
